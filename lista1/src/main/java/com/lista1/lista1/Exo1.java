d79613705cf596d717c915d6db12a91c3648a19a
Suru13 <06538972780@cefet-rj.br>
2024-08-30 21:32:56 -0300

    Resolução exercício1

---
+++ lista1/src/main/java/com/lista1/lista1/Exo1.java
@@ -0,0 +1,20 @@
+package com.lista1.lista1;
+
+public class Exo1 {
+    public static void resolucao1(){
+        int numero1;
+		int numero2;
+		System.out.print("Digite o numero1: \n");
+		Scanner scan = new Scanner(System.in);
+		numero1 = scan.nextInt();
+		System.out.print("O primeiro número é: "+numero1+"\n");
+
+		System.out.print("Digite o numero2: \n");
+		numero2 = scan.nextInt();
+		System.out.print("O primeiro número é: "+numero2+"\n");
+
+		System.out.print("A soma é : "+(numero1 + numero2)+"\n");
+		
+    }
+
+}
